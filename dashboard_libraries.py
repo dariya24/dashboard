17474d681753cbf2315b2c31af2796138b4e67a1
Dariia Reshetukha <darynareshetukha@gmail.com>
2024-10-12 15:01:45 +0200

    UI changes to Descriptive analytics tab
    Fix errors find during QA process
    Changed charts to be more interactive

--- dashboard_libraries.py
+++ dashboard_libraries.py
@@ -20,11 +20,69 @@ def get_pvalue_propotion(df, var1, var2):
     stat, pval = proportions_ztest(count, nobs)
     if pval < 0.005:
         significant = 1
-        text = "Statistically significant, pValue <0.005"
+        text = "Statistically significant, p-value <0.005"
         color = "green"
     else:
         significant = 0
-        text = "NOT statistically significant, pValue {0:.3f}".format(pval)
+        text = "NOT statistically significant, p-value {0:.3f}".format(pval)
         color = "red"
 
     return text, color
+
+
+
+def prepare_dataframe_for_descriptive_analytics(df):
+    df.rename(columns={"AGE": "Age", "GENDER": "Gender", "RURAL": "Rural or Urban", "Duration_Label": "Length of stay"}, inplace=True)
+
+    df.replace("LT7", "Less than 7 days", inplace=True)
+    df.replace("GE7", "7 or more days", inplace=True)
+
+    df.replace("F", "Female", inplace=True)
+    df.replace("M", "Male", inplace=True)
+
+    df.replace("R", "Rural", inplace=True)
+    df.replace("U", "Urban", inplace=True)
+
+    df.rename(
+        columns={
+            "TOTAL LEUKOCYTES COUNT": "Total Leukocytes Count",
+            "PLATELETS": "Platelets",
+            "GLUCOSE": "Glucose",
+            "UREA,CREATININE": "Urea,Creatinine"
+        }, inplace=True
+    )
+
+
+    cols_to_rename = ["SMOKING ", "ALCOHOL", "Diabetes Mellitus", "Hypertension", "Coronary Artery Disease",
+    "PRIOR CARDIOMYOPATHY", "CHRONIC KIDNEY DISEASE",
+    'SEVERE ANAEMIA', 'ANAEMIA', 'STABLE ANGINA',
+    'Acute coronary Syndrome', 'ST ELEVATION MYOCARDIAL INFARCTION',
+    'ATYPICAL CHEST PAIN', 'HEART FAILURE',
+    'HEART FAILURE WITH REDUCED EJECTION FRACTION',
+    'HEART FAILURE WITH NORMAL EJECTION FRACTION', 'Valvular Heart Disease',
+    'Complete Heart Block', 'Sick sinus syndrome', 'ACUTE KIDNEY INJURY',
+    'Cerebrovascular Accident INFRACT', 'Cerebrovascular Accident BLEED',
+    'Atrial Fibrilation', 'Ventricular Tachycardia',
+    'PAROXYSMAL SUPRA VENTRICULAR TACHYCARDIA', 'Congenital Heart Disease',
+    'Urinary tract infection', 'NEURO CARDIOGENIC SYNCOPE', 'ORTHOSTATIC',
+    'INFECTIVE ENDOCARDITIS', 'Deep venous thrombosis', 'CARDIOGENIC SHOCK',
+    'SHOCK', 'PULMONARY EMBOLISM', 'CHEST INFECTION']
+
+    new_cols_names = [x.lower().title() for x in cols_to_rename]
+
+    print(new_cols_names)
+
+    dict_cols = {}
+
+    for i in range(len(cols_to_rename)):
+        dict_cols[cols_to_rename[i]] = new_cols_names[i]
+
+
+
+    df.rename(
+        columns=dict_cols, inplace=True
+    )
+
+
+
+    return df
--- pages/1_Descriptive_analytics.py
+++ pages/1_Descriptive_analytics.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from dashboard_libraries import get_pvalue_propotion
+from dashboard_libraries import get_pvalue_propotion, prepare_dataframe_for_descriptive_analytics
+import plotly.figure_factory as ff
 
 
 st.set_page_config(
     page_title="PRAIS - Descriptive",
-    page_icon="👋",
+    page_icon="❤",
 )
 
 st.sidebar.image("./assets/P R A I S.png",)
@@ -15,62 +16,91 @@ st.sidebar.image("./assets/P R A I S.png",)
 
 df = pd.read_csv("data/HDHI_Admission_data_post_processed.csv")
 
+df = prepare_dataframe_for_descriptive_analytics(df)
+
 
 st.header("Demographic information")
 
+st.write("In this section the overview of the demographic in the dataset is presented")
 
 
 with st.container():
+    st.markdown("*Select this check box to only see results for patients who were admitted to ICU*")
     if st.checkbox('Only ICU Admissions '):
+
         subset = df[df.ICU_admission_status == 1]
     else:
         subset = df
 
-    # Group by 'Duration_Label' and 'GENDER' and count unique 'SNO'
-    grouped_df = subset.groupby(['Duration_Label', 'GENDER']).agg({'SNO': pd.Series.nunique}).reset_index()
-
-    # Rename the column to 'Unique_Encuonters_Count'
-    grouped_df = grouped_df.rename(columns={'SNO': 'Unique_Encounters_Count'})
-
-    # Calculate total unique SNO for each Duration_Label
-    total_counts = subset.groupby('Duration_Label')['SNO'].nunique().reset_index().rename(
-        columns={'SNO': 'Total_Unique_Encounters'})
-
-    # Merge with grouped data
-    merged_df = pd.merge(grouped_df, total_counts, on='Duration_Label')
-
-    # Calculate proportion of each gender within every Duration_Label
-    merged_df['Proportion'] = merged_df['Unique_Encounters_Count'] / merged_df['Total_Unique_Encounters']
-
-    st.table(data=merged_df.sort_values(by="Duration_Label", ascending=False))
 
     # Group by 'Duration_Label' and 'GENDER' and count unique 'SNO'
-    grouped_df = subset.groupby(['Duration_Label', 'RURAL']).agg({'SNO': pd.Series.nunique}).reset_index()
+    grouped_df = subset.groupby(['Length of stay', 'Gender']).agg({'SNO': pd.Series.nunique}).reset_index()
 
     # Rename the column to 'Unique_Encuonters_Count'
-    grouped_df = grouped_df.rename(columns={'SNO': 'Unique_Encounters_Count'})
+    grouped_df = grouped_df.rename(columns={'SNO': 'Number of Unique Encounters'})
 
     # Calculate total unique SNO for each Duration_Label
-    total_counts = subset.groupby('Duration_Label')['SNO'].nunique().reset_index().rename(
-        columns={'SNO': 'Total_Unique_Encounters'})
+    total_counts = subset.groupby('Length of stay')['SNO'].nunique().reset_index().rename(
+        columns={'SNO': 'Total Unique Encounters'})
 
     # Merge with grouped data
-    merged_df = pd.merge(grouped_df, total_counts, on='Duration_Label')
+    merged_df = pd.merge(grouped_df, total_counts, on='Length of stay')
 
     # Calculate proportion of each gender within every Duration_Label
-    merged_df['Proportion'] = merged_df['Unique_Encounters_Count'] / merged_df['Total_Unique_Encounters']
+    merged_df['Proportion'] = merged_df['Number of Unique Encounters'] / merged_df['Total Unique Encounters']
+    merged_df['Proportion'] = merged_df['Proportion'].apply(lambda x: "{}%".format(round(x * 100, 1)))
 
-    st.table(data=merged_df.sort_values(by="Duration_Label", ascending=False))
+    merged_df.drop(columns=['Total Unique Encounters'], inplace=True)
+    st.table(data=merged_df.sort_values(by="Length of stay", ascending=False))
 
-    subset.pivot(columns='Duration_Label', values="AGE").plot.hist(alpha=0.5, title="AGE", bins=50)
 
-    # Show the plot in Streamlit
-    st.pyplot(plt)
+    # Group by 'Duration_Label' and 'Rural or urban' and count unique 'SNO'
+    grouped_df = subset.groupby(['Length of stay', 'Rural or Urban']).agg({'SNO': pd.Series.nunique}).reset_index()
+
+    # Rename the column to 'Unique_Encuonters_Count'
+    grouped_df = grouped_df.rename(columns={'SNO': 'Number of Unique Encounters'})
+
+    # Calculate total unique SNO for each Duration_Label
+    total_counts = subset.groupby('Length of stay')['SNO'].nunique().reset_index().rename(
+        columns={'SNO': 'Total Unique Encounters'})
+
+    # Merge with grouped data
+    merged_df = pd.merge(grouped_df, total_counts, on='Length of stay')
+
+    # Calculate proportion of each gender within every Duration_Label
+    merged_df['Proportion'] = merged_df['Number of Unique Encounters'] / merged_df['Total Unique Encounters']
+
+    merged_df['Proportion'] = merged_df['Proportion'].apply(lambda x: "{}%".format(round(x*100, 1)))
+
+    merged_df.drop(columns=['Total Unique Encounters'], inplace=True)
+    st.table(data=merged_df.sort_values(by="Length of stay", ascending=False))
+
+    x1 = df[df["Length of stay"] == "Less than 7 days"].Age
+    x2 = df[df["Length of stay"] == "7 or more days"].Age
+    # Group data together
+    hist_data = [x1, x2]
+
+    group_labels = ['Less than 7 days', '7 or more days']
+
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(
+        hist_data, group_labels)
+
+    # Add title
+    fig.update_layout(title_text='Age Distribution')
+
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True)
+    #subset.pivot(columns='Length of stay', values="Age").plot.hist(alpha=0.5, title="Age Distribution", bins=50)
+    #st.pyplot(plt)
 
 
 st.header("Laboratory results")
 
-labs = ["Haemoglobin","TOTAL LEUKOCYTES COUNT","PLATELETS","GLUCOSE","UREA,CREATININE","B-TYPE NATRIURETIC PEPTIDE"]
+st.write("In this section the investigation of the laboratory tests results for various patient subgroups can be investigated")
+
+
+labs = ["Haemoglobin","Total Leukocytes Count","Platelets","Glucose","Urea,Creatinine"]
 
 
 
@@ -89,7 +119,7 @@ age_group  = st.selectbox(
     ["All", "<18", "18 - 35", "36 - 65", ">65"],
 )
 
-
+st.markdown("*Select this check box to only see results for patients who were admitted to ICU*")
 if st.checkbox('Only ICU Admissions'):
     only_ICU = 1
 else:
@@ -119,61 +149,75 @@ else:
     subset3 = subset2[subset2.AgeGroup == age_group]
 
 subset = subset3
-
+subset.dropna(inplace=True)
 
 number_encounters = subset.SNO.nunique()
 total_number_encounters = df.SNO.nunique()
 mean_value = subset[option].mean()
 median_value = subset[option].median()
 
+st.subheader("Results overview")
+
 st.write("Number of encounters: {0:.0f}".format(number_encounters))
-st.write("Percentage of all encounters in the dataset: {0:.2f}".format(100*number_encounters/total_number_encounters))
-st.write("Mean value: {0:.2f}".format(mean_value))
-st.write("Median value: {0:.2f}".format(median_value))
+st.write("Percentage of all encounters in the dataset that satisfy selected filters: {0:.1f}".format(100*number_encounters/total_number_encounters))
+st.write("Mean {} laboratory test value: {:.1f}".format(option, mean_value))
+st.write("Median {} laboratory test value: {:.1f}".format(option, median_value))
+
+
+x1 = df[df["Length of stay"] == "Less than 7 days"][option]
+x2 = df[df["Length of stay"] == "7 or more days"][option]
+# Group data together
+hist_data = [x1, x2]
+
+group_labels = ['Less than 7 days', '7 or more days']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+    hist_data, group_labels, bin_size=[0.25, 0.25])
+
+# Add title
+fig.update_layout(title_text='{} Distribution'.format(option))
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
 
 
 
-col1, col2 = st.columns(2)
+#col1, col2 = st.columns(2)
 
-with col1:
+#with col1:
 
-    subset.pivot(columns='Duration_Label', values=option).plot.hist(alpha=0.5, title=option, bins=50)
+#subset.pivot(columns='Length of stay', values=option).plot.hist(alpha=0.5, title=option, bins=50)
 
-    # Show the plot in Streamlit
-    st.pyplot(plt)
+# Show the plot in Streamlit
+#st.pyplot(plt)
 
 
-with col2:
-
-    reference_values = pd.DataFrame(np.array([["<18", "X", "X"], ["18-35", "X", "X"], ["35-65", "X", "X"], ["65+", "X", "X"]]),
-                 columns=['Age Group', 'Male', 'Female'])
-
-    st.write("Reference value for: {}".format(option))
-    st.table(data=reference_values)
+#with col2:
+#
+#    reference_values = pd.DataFrame(np.array([["<18", "X", "X"], ["18-35", "X", "X"], ["35-65", "X", "X"], ["65+", "X", "X"]]),
+#                 columns=['Age Group', 'Male', 'Female'])
+#
+#    st.write("Reference value for: {}".format(option))
+#    st.table(data=reference_values)
 
 st.header("Comorbidities")
-
-
+st.write("In this section the investigation of the comorbidities for various patient subgroups can be investigated")
 
 with st.container():
 
     #conditions = ["boolGender", "boolRural", "SMOKING ","ALCOHOL","Diabetes Mellitus","Hypertension","Coronary Artery Disease","PRIOR CARDIOMYOPATHY","CHRONIC KIDNEY DISEASE"]
 
 
-    conditions = ["boolGender", "boolRural", "SMOKING ","ALCOHOL","Diabetes Mellitus","Hypertension","Coronary Artery Disease",
-                  "PRIOR CARDIOMYOPATHY","CHRONIC KIDNEY DISEASE",
-   'SEVERE ANAEMIA', 'ANAEMIA', 'STABLE ANGINA',
-       'Acute coronary Syndrome', 'ST ELEVATION MYOCARDIAL INFARCTION',
-       'ATYPICAL CHEST PAIN', 'HEART FAILURE',
-       'HEART FAILURE WITH REDUCED EJECTION FRACTION',
-       'HEART FAILURE WITH NORMAL EJECTION FRACTION', 'Valvular Heart Disease',
-       'Complete Heart Block', 'Sick sinus syndrome', 'ACUTE KIDNEY INJURY',
-       'Cerebrovascular Accident INFRACT', 'Cerebrovascular Accident BLEED',
-       'Atrial Fibrilation', 'Ventricular Tachycardia',
-       'PAROXYSMAL SUPRA VENTRICULAR TACHYCARDIA', 'Congenital Heart Disease',
-       'Urinary tract infection', 'NEURO CARDIOGENIC SYNCOPE', 'ORTHOSTATIC',
-       'INFECTIVE ENDOCARDITIS', 'Deep venous thrombosis', 'CARDIOGENIC SHOCK',
-       'SHOCK', 'PULMONARY EMBOLISM', 'CHEST INFECTION']
+    conditions = ['Smoking ', 'Alcohol', 'Diabetes Mellitus', 'Hypertension', 'Coronary Artery Disease', 'Prior Cardiomyopathy', 'Chronic Kidney Disease',
+                  'Severe Anaemia', 'Anaemia', 'Stable Angina', 'Acute Coronary Syndrome',
+'St Elevation Myocardial Infarction', 'Atypical Chest Pain', 'Heart Failure', 'Heart Failure With Reduced Ejection Fraction',
+                  'Heart Failure With Normal Ejection Fraction', 'Valvular Heart Disease', 'Complete Heart Block', 'Sick Sinus Syndrome',
+                  'Acute Kidney Injury', 'Cerebrovascular Accident Infract', 'Cerebrovascular Accident Bleed', 'Atrial Fibrilation',
+                  'Ventricular Tachycardia', 'Paroxysmal Supra Ventricular Tachycardia', 'Congenital Heart Disease',
+                  'Urinary Tract Infection', 'Neuro Cardiogenic Syncope', 'Orthostatic', 'Infective Endocarditis',
+                  'Deep Venous Thrombosis', 'Cardiogenic Shock', 'Shock', 'Pulmonary Embolism', 'Chest Infection']
+
 
     option = st.selectbox(
         "Which comorbidity you want to review?",
@@ -185,6 +229,7 @@ with st.container():
     # Show the plot in Streamlit
     # st.pyplot(plt)
 
+    st.markdown("*Select this check box to only see results for patients who were admitted to ICU*")
     if st.checkbox('Only ICU Admissions  '):
         only_ICU1 = 1
         subset = df[df.ICU_admission_status == 1]
@@ -192,24 +237,43 @@ with st.container():
         only_ICU1 = 0
         subset = df
 
+    st.write("The tables below show the number and proportion of individuals with selected comorbidity")
+    st.write("Statistically significant comorbidities are colored green and others - red")
 
-    a = subset.groupby("Duration_Label")[option].sum().reset_index()
-    b = subset.groupby("Duration_Label").SNO.count()
-    x = pd.merge(a, b, on="Duration_Label")
-    x["Perc"] = (x[option] / x['SNO']) * 100
+    st.subheader("Comparison of {} for different length of stay".format(option))
 
-    st.table(data=x.sort_values(by="Duration_Label", ascending=False))
-    text, color = get_pvalue_propotion(subset, option, "Duration_Label")
+    a = subset.groupby("Length of stay")[option].sum().reset_index()
+    b = subset.groupby("Length of stay").SNO.count()
+    x = pd.merge(a, b, on="Length of stay")
+    x["Proportion"] = (x[option] / x['SNO']) * 100
+    x["Proportion"] = x["Proportion"].apply(lambda x: "{}%".format(round(x)))
+    x.drop(columns={"SNO"}, inplace=True)
+    st.table(data=x.sort_values(by="Length of stay", ascending=False))
+
+    text, color = get_pvalue_propotion(subset, option, "Length of stay")
     st.write(":{}[{}]".format(color, text))
+
+    st.subheader("Comparison of {} for different ICU Admission".format(option))
 
     a = subset.groupby("ICU_admission_status")[option].sum().reset_index()
     b = subset.groupby("ICU_admission_status").SNO.count()
     x = pd.merge(a, b, on="ICU_admission_status")
-    x["Perc"] = (x[option] / x['SNO']) * 100
+    x["Proportion"] = (x[option] / x['SNO']) * 100
+    x["Proportion"] = x["Proportion"].apply(lambda x: "{}%".format(round(x)))
+    x.drop(columns={"SNO"}, inplace=True)
+
+    x["ICU_admission_status"] = x["ICU_admission_status"].apply(lambda x: "Admitted to ICU" if x == 1 else "Not admitted to ICU")
+    x.rename(columns={"ICU_admission_status": "ICU Admission Status"})
     st.table(data=x)
+
+
+
 
     # If only ICU, then we can not calculate percentage, therefore only do this line below if NOT ICU
     if not only_ICU1:
         text, color = get_pvalue_propotion(subset, option, "ICU_admission_status")
         st.write(":{}[{}]".format(color, text))
 
+
+import plotly
+print(plotly.__version__)
